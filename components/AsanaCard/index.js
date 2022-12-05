54a978cb55e113c84bbc11084783d734277eeaeb
Nimalan Suntharalingam <nimalan@posteo.de>
2022-12-05 14:51:24 +0100

    addButton

--- components/AsanaCard/index.js
+++ components/AsanaCard/index.js
@@ -1,33 +1,95 @@
 import styled from "styled-components";
+import Image from "next/image";
+import Check from "../../public/Images/noun-check-mark-1550146.svg";
+import Close from "../../public/Images/noun-close-1386186.svg";
 
 export default function AsanaCard({asana}) {
   return (
-    <article>
-      <Image
-        width={2000}
-        height={5000}
+    <Article>
+      <StyledImage
+        width="200"
+        height="600"
         src={asana.image}
-        alt={`yoga pose: ${asana.name}`}
+        alt={`yoga pose: ${asana.sanskrit}`}
         priority
       />
       <StyledAsanaCard>{AsanaCard}</StyledAsanaCard>
-      <h2>{asana.name}</h2>
-    </article>
+      <StyledSanskrit>{asana.sanskrit}</StyledSanskrit>
+      <StyledSanskrit>{asana.sanskrit2}</StyledSanskrit>
+      <ButtonContainer>
+        <Button>
+          <Image
+            style={{position: "relative", top: "8px"}}
+            src={Close}
+            width="40"
+            height="40"
+            alt={"close"}
+          />
+        </Button>
+        <ButtonContent></ButtonContent>
+        <Button>
+          <Image
+            style={{position: "relative", top: "8px"}}
+            src={Check}
+            width="40"
+            height="40"
+            alt={"checkmark"}
+          />
+        </Button>
+      </ButtonContainer>
+    </Article>
   );
 }
 
 const StyledAsanaCard = styled.article`
   margin: 0;
-  padding: 3rem 3rem 3rem 3rem;
-  min-height: calc(100vh - 8rem);
-  margin-top: 0;
+  margin-top: 10px;
 `;
 
-const Image = styled.img`
-  padding: 1rem 1rem 1rem 1rem;
-  border-radius: 120px;
-  box-shadow: 0 2px 5px 2px rgba(0, 0, 0, 0.45);
-  width: 600px;
-  height: 1000px;
-  margin: 45%;
+const StyledImage = styled(Image)`
+  padding: 18rem 1rem 0 1rem;
+  width: 300px;
+`;
+const Article = styled.article`
+  border-radius: 75px;
+  background: rgba(255, 255, 255, 0.12);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(3.4px);
+  -webkit-backdrop-filter: blur(3.4px);
+  border: 1px solid rgba(255, 255, 255, 0.49);
+  text-align: center;
+  margin: 1rem auto 0 auto;
+  width: 75%;
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Button = styled.div`
+  border-radius: 100%;
+  width: 50px;
+  height: 50px;
+  margin: 0px 35px;
+  background: rgba(255, 255, 255, 0.02);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(1.5px);
+  -webkit-backdrop-filter: blur(1.5px);
+  border: 1px solid rgba(255, 255, 255, 0.49);
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  padding-top: 15px;
+  margin-bottom: 50px;
+`;
+
+const ButtonContent = styled.div``;
+
+const StyledSanskrit = styled.span`
+  font-size: 12px;
+  color: lightblue;
+  padding-bottom: 20px;
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import AsanaCard from "../components/AsanaCard";
 import asanas from "./db.json";
 import {useState, useEffect} from "react";
+import styled from "styled-components";
+import flowLogo from "../public/Images/0_YogaFlow_Logo.svg";
+import Image from "next/image";
 
 export default function Home() {
   const [asana, setAsana] = useState(null);
@@ -14,11 +17,39 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="App">
-      <header>
-        <h1>Flow</h1>
+    <>
+      <Background />
+      <header style={{display: "grid", placeContent: "center"}}>
+        <YogaFlowLogo
+          src={flowLogo}
+          width="120"
+          height="120"
+          alt={"0_YogaFlowLogo"}
+        />
       </header>
       {asana && <AsanaCard asana={asana} />}
-    </div>
+    </>
   );
 }
+const Background = styled.main`
+  background: rgb(0, 113, 188);
+  background: linear-gradient(
+    45deg,
+    rgba(0, 113, 188, 1) 25%,
+    rgba(63, 143, 213, 1) 35%,
+    rgba(53, 87, 117, 1) 90%
+  );
+  z-index: -1;
+  width: 100vw;
+  height: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const YogaFlowLogo = styled(Image)`
+  display: grid;
+  place-content: center;
+`;
