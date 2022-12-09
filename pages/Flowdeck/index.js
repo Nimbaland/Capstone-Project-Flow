a5c35d119b45dcc7ff67e923c1872621b9faed18
Nimalan Suntharalingam <nimalan@posteo.de>
2022-12-09 10:00:39 +0100

    declearParsingError

--- pages/Flowdeck/index.js
+++ pages/Flowdeck/index.js
@@ -1,23 +1,25 @@
 import styled from "styled-components";
 import AsanaCard from "../../components/AsanaCard";
 
-export default function FavoriteAsanaCard({ asanas, setAsana }) {
-    function deleteSpot(id) {
-      setAsana((asanas) => {
-        const newAsanaCardList = asanas.filter((asanas) => asanas.id !== id);
-        return newAsanaCardList;
-      });
-    }
+export default function FavoriteAsanaCard({asanas, setAsana}) {
+  function deleteSpot(id) {
+    setAsana(asanas => {
+      const newAsanaCardList = asanas.filter(asanas => asanas.id !== id);
+      return newAsanaCardList;
+    });
+  }
 
-    function toggleFavorite(id) {
-        const newAsanaCardArray = asanas.map((asana) => {
-          if (asanas.id === id) {
-            return {
-              ...asanas,
-              isFavorite: !asanas.isFavorite,
-            };
-          } else {
-            return asanas
-          }
-        });
-        setAsana(newAsanaCardArray);
+  function toggleFavorite(id) {
+    const newAsanaCardArray = asanas.map(asana => {
+      if (asanas.id === id) {
+        return {
+          ...asanas,
+          isFavorite: !asanas.isFavorite,
+        };
+      } else {
+        return asanas;
+      }
+    });
+    setAsana(newAsanaCardArray);
+  }
+}
